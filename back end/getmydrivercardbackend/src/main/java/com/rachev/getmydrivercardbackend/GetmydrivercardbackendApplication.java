314f5e9fd4ce9ac58fad13718abc4e9ac1e06b7d
Ivan Rachev <ivan.rachev97@gmail.com>
2018-10-15 15:41:42 +0300

    plain projects creation

---
+++ back end/getmydrivercardbackend/src/main/java/com/rachev/getmydrivercardbackend/GetmydrivercardbackendApplication.java
@@ -0,0 +1,12 @@
+package com.rachev.getmydrivercardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetmydrivercardbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetmydrivercardbackendApplication.class, args);
+	}
+}
---
+++ back end/getmydrivercardbackend/src/test/java/com/rachev/getmydrivercardbackend/GetmydrivercardbackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rachev.getmydrivercardbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GetmydrivercardbackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
